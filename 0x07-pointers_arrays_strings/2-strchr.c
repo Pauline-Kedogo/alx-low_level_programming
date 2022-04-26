3771ce1e869ff7b175f9d849c104679ecb1d05f6
Pauline Kedogo <paulinerumama@gmail.com>
2022-04-26 06:41:24 -0700

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
  *  _strchr - finds the first occurence of character
  *  @s: string to search
@@ -8,7 +6,6 @@
  *  Return: On success - char pointer pointing to the
  *  memory occurrence of c; On failure - NULL.
  */
-
 char *_strchr(char *s, char c)
 {
 	int i;

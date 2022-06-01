94cefdef6bce056a058cee37998f2a3798002252
Pauline Kedogo <paulinerumama@gmail.com>
2022-06-01 04:32:49 -0700

    checks the endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * get_endianness - checks for endianness
+ * Description: endianness is how a computer stores
+ * and reads multibyte data types
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+{
+	int i = 1;
+	char *c = (char *)&i;
+
+	i = (*c) ? 1 : 0;
+	return (i);
+}

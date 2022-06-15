b61e57848cb2ea50e667163251378058411c1451
Pauline Kedogo <paulinerumama@gmail.com>
2022-06-15 13:19:20 -0700

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -16,7 +16,7 @@ void print_all(const char * const format, ...)
 	va_start(arguments, format);
 	while (format && format[i])
 		{
-			switch (format[i])
+			switch (format[i++])
 			{
 			case 'c':
 				printf("%c", va_arg(arguments, int));
